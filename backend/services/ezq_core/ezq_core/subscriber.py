18078b6d989ade11f7add09d4761c00e90c2a55c
YangYang <this@pro-yy.com>
2017-11-23 17:04:16 +0800

    manual ack
    Signed-off-by: YangYang <this@pro-yy.com>

--- backend/services/ezq_core/ezq_core/subscriber.py
+++ backend/services/ezq_core/ezq_core/subscriber.py
@@ -6,13 +6,15 @@ from ezq_core.dispatcher import dispatch
 from ezq_core.logger import logger
 
 async def on_message(message: IncomingMessage):
-    with message.process():     # auto message.ack()
-        body = loads(message.body)
-        event = body['event']
-        print('# {} [received] sleep 1 sec...'.format(event))
-        await asyncio.sleep(1) # Represents async I/O operations
-        del body['event']
-        await dispatch(event, body)
+    body = loads(message.body)
+    event = body['event']
+    print('# {} [received] sleep 1 sec...'.format(event))
+    await asyncio.sleep(1) # Represents async I/O operations
+    del body['event']
+    await dispatch(event, body)
+    # manual ack here
+    # not with message.process() context, which auto ack even when error
+    message.ack()
 
 async def start_subscribe(url, *args, **kwargs):
     connection = await connect_robust(url)
